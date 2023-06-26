3d29f3da06eb8a5c24ac6fd4fcae8e48fe9f7963
Igor-Yngvi <96557071+Igor-Yngvi@users.noreply.github.com>
2023-06-26 00:19:48 -0300

    Add files via upload

--- scripts/autocompletar.js
+++ scripts/autocompletar.js
@@ -1,8 +1,8 @@
 // Lista de sugestões por linguagem
 const suggestions = {
-  html: ["<!DOCTYPE html>", "<html>", "<head>", "<body>", "<h1>", "<p>", "<div>", "<span>", "<img>", "<a>"],
-  css: ["background-color", "color", "font-family", "margin", "padding", "border", "width", "height"],
-  js: ["console.log()", "function", "if", "for", "while", "var", "const", "let"]
+  html: [ "<a>", "<abbr>", "<acronym>", "<address>", "<applet>", "<area>", "<article>", "<aside>", "<audio>", "<b>", "<base>", "<basefont>", "<bdi>", "<bdo>", "<big>", "<blockquote>", "<body>", "<br>", "<button>", "<canvas>", "<caption>", "<center>", "<cite>", "<code>", "<col>", "<colgroup>", "<data>", "<datalist>", "<dd>", "<del>", "<details>", "<dfn>", "<dialog>", "<dir>", "<div>", "<dl>", "<dt>", "<em>", "<embed>", "<fieldset>", "<figcaption>", "<figure>", "<font>", "<footer>", "<form>", "<frame>", "<frameset>", "<h1>", "<h2>", "<h3>", "<h4>", "<h5>", "<h6>", "<head>", "<header>", "<hr>", "<html>", "<i>", "<iframe>", "<img>", "<input>", "<ins>", "<kbd>", "<label>", "<legend>", "<li>", "<link>", "<main>", "<map>", "<mark>", "<meta>", "<meter>", "<nav>", "<noframes>", "<noscript>", "<object>", "<ol>", "<optgroup>", "<option>", "<output>", "<p>", "<param>", "<picture>", "<pre>", "<progress>", "<q>", "<rp>", "<rt>", "<ruby>", "<s>", "<samp>", "<script>", "<section>", "<select>", "<small>", "<source>", "<span>", "<strike>", "<strong>", "<style>", "<sub>", "<summary>", "<sup>", "<svg>", "<table>", "<tbody>", "<td>", "<template>", "<textarea>", "<tfoot>", "<th>", "<thead>", "<time>", "<title>", "<tr>", "<track>", "<tt>", "<u>", "<ul>", "<var>", "<video>", "<wbr>" ],
+  css: [ "align-content", "align-items", "align-self", "all", "animation", "animation-delay", "animation-direction", "animation-duration", "animation-fill-mode", "animation-iteration-count", "animation-name", "animation-play-state", "animation-timing-function", "backdrop-filter", "backface-visibility", "background", "background-attachment", "background-blend-mode", "background-clip", "background-color", "background-image", "background-origin", "background-position", "background-repeat", "background-size", "block-size", "border", "border-block", "border-block-color", "border-block-end", "border-block-end-color", "border-block-end-style", "border-block-end-width", "border-block-start", "border-block-start-color", "border-block-start-style", "border-block-start-width", "border-block-style", "border-block-width", "border-bottom", "border-bottom-color", "border-bottom-left-radius", "border-bottom-right-radius", "border-bottom-style", "border-bottom-width", "border-collapse", "border-color", "border-image", "border-image-outset", "border-image-repeat", "border-image-slice", "border-image-source", "border-image-width", "border-inline", "border-inline-color", "border-inline-end", "border-inline-end-color", "border-inline-end-style", "border-inline-end-width", "border-inline-start", "border-inline-start-color", "border-inline-start-style", "border-inline-start-width", "border-inline-style", "border-inline-width", "border-left", "border-left-color", "border-left-style", "border-left-width", "border-radius", "border-right", "border-right-color", "border-right-style", "border-right-width", "border-spacing", "border-style", "border-top", "border-top-color", "border-top-left-radius", "border-top-right-radius", "border-top-style", "border-top-width", "border-width", "bottom", "box-decoration-break", "box-shadow", "box-sizing", "break-after", "break-before", "break-inside", "caption-side", "caret-color", "clear", "clip", "clip-path", "color", "column-count", "column-fill", "column-gap", "column-rule", "column-rule-color", "column-rule-style", "column-rule-width", "column-span", "column-width", "columns", "content", "counter-increment", "counter-reset", "cue", "cue-after", "cue-before", "cursor", "direction", "display", "empty-cells", "filter", "flex", "flex-basis", "flex-direction", "flex-flow", "flex-grow", "flex-shrink", "flex-wrap", "float", "font", "font-family", "font-feature-settings", "font-kerning", "font-language-override", "font-optical-sizing", "font-size", "font-size-adjust", "font-stretch", "font-style", "font-synthesis", "font-variant", "font-variant-alternates", "font-variant-caps", "font-variant-east-asian", "font-variant-ligatures", "font-variant-numeric", "font-variant-position", "font-weight", "gap", "grid", "grid-area", "grid-auto-columns", "grid-auto-flow", "grid-auto-rows", "grid-column", "grid-column-end", "grid-column-start", "grid-row", "grid-row-end", "grid-row-start", "grid-template", "grid-template-areas", "grid-template-columns", "grid-template-rows", "hanging-punctuation", "height", "hyphens", "image-orientation", "image-rendering", "image-resolution", "ime-mode", "initial-letter", "initial-letter-align", "inline-size", "inset", "inset-block", "inset-block-end", "inset-block-start", "inset-inline", "inset-inline-end", "inset-inline-start", "isolation", "justify-content", "justify-items", "justify-self", "left", "letter-spacing", "line-break", "line-height", "list-style", "list-style-image", "list-style-position", "list-style-type", "margin", "margin-block", "margin-block-end", "margin-block-start", "margin-bottom", "margin-inline", "margin-inline-end", "margin-inline-start", "margin-left", "margin-right", "margin-top", "mask", "mask-border", "mask-border-mode", "mask-border-outset", "mask-border-repeat", "mask-border-slice", "mask-border-source", "mask-border-width", "mask-clip", "mask-composite", "mask-image", "mask-mode", "mask-origin", "mask-position", "mask-repeat", "mask-size", "mask-type", "max-block-size", "max-height", "max-inline-size", "max-width", "min-block-size", "min-height", "min-inline-size", "min-width", "mix-blend-mode", "object-fit", "object-position", "offset", "offset-anchor", "offset-distance", "offset-path", "offset-position", "offset-rotate", "opacity", "order", "orphans", "outline", "outline-color", "outline-offset", "outline-style", "outline-width", "overflow", "overflow-anchor", "overflow-block", "overflow-clip-box", "overflow-inline", "overflow-wrap", "overflow-x", "overflow-y", "overscroll-behavior", "overscroll-behavior-block", "overscroll-behavior-inline", "overscroll-behavior-x", "overscroll-behavior-y", "padding", "padding-block", "padding-block-end", "padding-block-start", "padding-bottom", "padding-inline", "padding-inline-end", "padding-inline-start", "padding-left", "padding-right", "padding-top", "page-break-after", "page-break-before", "page-break-inside", "perspective", "perspective-origin", "place-content", "place-items", "place-self", "pointer-events", "position", "quotes", "resize", "right", "rotate", "row-gap", "ruby-align", "ruby-merge", "ruby-position", "scale", "scroll-behavior", "scroll-margin", "scroll-margin-block", "scroll-margin-block-end", "scroll-margin-block-start", "scroll-margin-bottom", "scroll-margin-inline", "scroll-margin-inline-end", "scroll-margin-inline-start", "scroll-margin-left", "scroll-margin-right", "scroll-margin-top", "scroll-padding", "scroll-padding-block", "scroll-padding-block-end", "scroll-padding-block-start", "scroll-padding-bottom", "scroll-padding-inline", "scroll-padding-inline-end", "scroll-padding-inline-start", "scroll-padding-left", "scroll-padding-right", "scroll-padding-top", "scroll-snap-align", "scroll-snap-stop", "scroll-snap-type", "shape-image-threshold", "shape-margin", "shape-outside", "tab-size", "table-layout", "text-align", "text-align-last", "text-combine-upright", "text-decoration", "text-decoration-color", "text-decoration-line", "text-decoration-skip-ink", "text-decoration-style", "text-decoration-thickness", "text-emphasis", "text-emphasis-color", "text-emphasis-position", "text-emphasis-style", "text-indent", "text-justify", "text-orientation", "text-overflow", "text-rendering", "text-shadow", "text-size-adjust", "text-transform", "text-underline-position", "top", "transform", "transform-box", "transform-origin", "transform-style", "transition", "transition-delay", "transition-duration", "transition-property", "transition-timing-function", "translate", "unicode-bidi", "user-select", "vertical-align", "visibility", "white-space", "widows", "width", "will-change", "word-break", "word-spacing", "word-wrap", "writing-mode", "z-index" ],
+  js: [ "abstract", "arguments", "Array", "async", "await", "BigInt", "boolean", "break", "byte", "case", "catch", "char", "class", "const", "continue", "debugger", "default", "delete", "do", "double", "else", "enum", "eval", "export", "extends", "false", "final", "finally", "float", "for", "function", "goto", "if", "implements", "import", "in", "instanceof", "int", "interface", "let", "long", "Math", "NaN", "native", "new", "null", "Number", "Object", "package", "private", "protected", "public", "return", "short", "static", "super", "switch", "synchronized", "this", "throw", "throws", "transient", "true", "try", "typeof", "undefined", "var", "void", "volatile", "while", "with", "yield" ]
 };
 
 // Função para exibir as sugestões
@@ -20,9 +20,12 @@ function showSuggestions() {
     return;
   }
 
-  // Filtrar sugestões com base no texto atual
+  // Verificar a palavra atual
+  const currentWord = getCurrentWord(editorContent);
+
+  // Filtrar sugestões com base na palavra atual
   const filteredSuggestions = suggestions[selectedLanguage].filter((suggestion) => {
-    return suggestion.startsWith(editorContent);
+    return suggestion.startsWith(currentWord);
   });
 
   // Exibir as sugestões
@@ -31,42 +34,79 @@ function showSuggestions() {
     suggestionItem.classList.add("suggestion-item");
     suggestionItem.textContent = suggestion;
 
-    suggestionItem.addEventListener("click", () => {
+    suggestionItem.addEventListener("mousedown", () => {
       insertSuggestion(suggestion);
+    });
+
+    suggestionItem.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        insertSuggestion(suggestion);
+      }
     });
 
     suggestionsContainer.appendChild(suggestionItem);
   });
 }
 
-// Função para inserir a sugestão no editor
-function insertSuggestion(suggestion) {
-  const editor = document.getElementById("editor");
-  const editorContent = editor.textContent;
+// Função para obter a palavra atual
+function getCurrentWord(text) {
   const selection = window.getSelection();
   const range = selection.getRangeAt(0);
 
-  const startNode = range.startContainer;
-  const startOffset = range.startOffset;
-  const endNode = range.endContainer;
-  const endOffset = range.endOffset;
+  // Obter o conteúdo completo do editor
+  const editorContent = document.getElementById("editor").textContent;
 
-  const beforeText = editorContent.slice(0, startOffset);
-  const afterText = editorContent.slice(endOffset);
+  // Verificar a posição do início e fim da seleção
+  const start = range.startOffset;
+  const end = range.endOffset;
 
-  editor.textContent = beforeText + suggestion + afterText;
+  // Verificar o início e fim da palavra atual
+  let wordStart = start;
+  while (wordStart > 0 && !/\s/.test(editorContent[wordStart - 1])) {
+    wordStart--;
+  }
 
-  // Posicionar o cursor após a inserção da sugestão
-  const newRange = document.createRange();
-  const newSelection = window.getSelection();
+  let wordEnd = end;
+  while (wordEnd < editorContent.length && !/\s/.test(editorContent[wordEnd])) {
+    wordEnd++;
+  }
 
-  newRange.setStartAfter(endNode);
-  newRange.setEndAfter(endNode);
+  // Retornar a palavra atual
+  return editorContent.slice(wordStart, wordEnd);
+}
 
-  newSelection.removeAllRanges();
-  newSelection.addRange(newRange);
+// Função para inserir a sugestão no editor
+function insertSuggestion(suggestion) {
+  const editor = document.getElementById("editor");
+  const selection = window.getSelection();
+  const range = selection.getRangeAt(0);
+
+  // Remover o texto selecionado
+  range.deleteContents();
+
+  // Inserir a sugestão no lugar do texto removido
+  const suggestionNode = document.createTextNode(suggestion);
+  range.insertNode(suggestionNode);
+
+  // Colocar o cursor no final da sugestão
+  range.setStartAfter(suggestionNode);
+  range.collapse(true);
+  selection.removeAllRanges();
+  selection.addRange(range);
+
+  // Limpar as sugestões
+  const suggestionsContainer = document.getElementById("suggestions-container");
+  suggestionsContainer.innerHTML = "";
+
+  // Chamar a função de exibição de sugestões novamente após a inserção
+  showSuggestions();
 }
 
 // Evento de digitação no editor
-const editor = document.getElementById("editor");
-editor.addEventListener("input", showSuggestions);
+document.getElementById("editor").addEventListener("input", handleInput);
+
+// Função de tratamento do evento de digitação
+function handleInput() {
+  // Chamar a função de exibição de sugestões após um pequeno atraso
+  setTimeout(showSuggestions, 50);
+}
